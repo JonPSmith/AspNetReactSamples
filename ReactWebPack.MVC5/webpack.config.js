45407e8b061d2af4fd98546f23cbf0f511d937f9
Jon Smith <jon.smith@selectiveanalytics.com>
2016-05-02 20:01:46 +0100

    Changed WebPack to have two files: main and vendor

--- ReactWebPack.MVC5/webpack.config.js
+++ ReactWebPack.MVC5/webpack.config.js
@@ -1,18 +1,40 @@
+var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var pkg = require('./package.json');
+
+// bundle dependencies in separate vendor bundle
+var vendorPackages = Object.keys(pkg.dependencies).filter(function (el) {
+    return el.indexOf('font') === -1; // exclude font packages from vendor bundle
+});
 
 /*
  * Default webpack configuration for development
  */
 var config = {
   devtool: 'eval-source-map',
-  entry:  __dirname + '/app/App.js',
+  entry: {
+      main: path.join(__dirname, '/app/App.js'),
+      vendor: vendorPackages
+  },
   output: {
-    path: __dirname + '/js/',
-    filename: 'bundle.js'
+      path: path.join(__dirname, '/js/'),
+      filename: '[name].js',
+      sourceMapFilename: "[file].map"
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js',
+        minChunks: Infinity
+    })
+  ],
+  resolveLoader: {
+      'fallback': path.join(__dirname, 'node_modules')
   },
   module: {
     loaders: [{
-      test: /\.jsx?$/,
+      test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel',
       query: {
