99b0e60699c54ac8fb5abdf94f7a548a3d79d951
Jon Smith <jon.smith@selectiveanalytics.com>
2016-05-05 11:37:34 +0100

    Added base ASP.NET MVC5 project

---
+++ ReactJsNet.MVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReactJsNet.MVC5.Startup))]
+namespace ReactJsNet.MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
