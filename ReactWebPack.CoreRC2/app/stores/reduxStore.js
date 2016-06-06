c8bcb3e613ce11aea5c307b1e8ab7f1f567a13a1
Jon Smith <jon.smith@selectiveanalytics.com>
2016-06-06 10:18:42 +0100

    Added redux-trottle to handle trottle of some actions

--- ReactWebPack.CoreRC2/app/stores/reduxStore.js
+++ ReactWebPack.CoreRC2/app/stores/reduxStore.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import throttle from "redux-throttle";
 import reducers from '../reducers/index';
- 
+
+const defaultThrottleOption = { // https://lodash.com/docs#throttle
+  leading: true,
+  trailing: false
+}
+
+const throttleMiddleWare = throttle(500, defaultThrottleOption);    //default 500ms, 
+
 const reduxStore = createStore(
    reducers,
-   applyMiddleware(thunk) 
+   applyMiddleware(thunk, throttleMiddleWare) 
 );
 
 export default reduxStore;
