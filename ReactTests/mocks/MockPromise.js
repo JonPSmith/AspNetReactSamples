ce58e8eb67d354f624a4965e965fdf0c047433bf
Jon Smith <jon.smith@selectiveanalytics.com>
2016-06-25 09:41:48 +0100

    Comment to say its a limited mock of a promise

--- ReactTests/mocks/MockPromise.js
+++ ReactTests/mocks/MockPromise.js
@@ -1,4 +1,8 @@
-﻿class MockPromise {
+﻿//This is a super-simple mock of a JavaScript Promise
+//It only implement the 'then(success, failure)' function 
+//as that is the only function that the kanban calls
+//in the modules that use the KanbanApi    
+class MockPromise {
   constructor(returnSuccess, result) {
         this.returnSuccess = returnSuccess;
         this.result = result || (returnSuccess ? 'my data': 'my error')
