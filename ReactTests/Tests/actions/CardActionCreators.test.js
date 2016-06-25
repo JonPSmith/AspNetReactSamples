44ff0aa2128c1747d19e42f7fa23c874a83d14d2
Jon Smith <jon.smith@selectiveanalytics.com>
2016-06-25 17:52:10 +0100

    Added amount test

--- ReactTests/Tests/actions/CardActionCreators.test.js
+++ ReactTests/Tests/actions/CardActionCreators.test.js
@@ -3,11 +3,13 @@ import MockPromise from '../../mocks/MockPromise';
 
 import constants from '../../../ReactWebPack.CoreRC2/app/constants';
 
-describe('CardActionCreators', () => {
-    let returnedActionObjects = [];
+describe('CardActionCreators, success', () => {
     const mockKanbanAPISuccess = {
         fetchCards() {
             return new MockPromise(true)
+        },
+        addCard(card) {
+            return new MockPromise(true, {fromServer: 'server side'})
         }
     };
 
@@ -19,6 +21,7 @@ describe('CardActionCreators', () => {
     describe('fetchCards', () => {
 
         it('success', () => {
+            let returnedActionObjects = [];
             cardActions.fetchCards()((actionObject) => {
                 returnedActionObjects.push( actionObject);
             });
@@ -26,6 +29,22 @@ describe('CardActionCreators', () => {
             expect(returnedActionObjects[0].type).toEqual(constants.FETCH_CARDS);
             expect(returnedActionObjects[1].type).toEqual(constants.FETCH_CARDS_SUCCESS);
             expect(returnedActionObjects[1].payload.response).toEqual('my data');
+        });
+    });
+
+    describe('addCard', () => {
+
+        it('success', () => {
+            let returnedActionObjects = [];
+            cardActions.addCard({ fromReact: 'client side'})((actionObject) => {
+                returnedActionObjects.push( actionObject);
+            });
+            debugger;
+            expect(returnedActionObjects.length).toEqual(2);
+            expect(returnedActionObjects[0].type).toEqual(constants.CREATE_CARD);
+            expect(returnedActionObjects[1].type).toEqual(constants.CREATE_CARD_SUCCESS);
+            expect(returnedActionObjects[1].payload.card.fromReact).toEqual('client side');
+            expect(returnedActionObjects[1].payload.response.fromServer).toEqual('server side');
         });
     });
 }); 
