02a31445f71770a2327e647d466b374c2752d3df
Jon Smith <jon.smith@selectiveanalytics.com>
2016-06-25 18:13:56 +0100

    Added some more tests

--- ReactTests/Tests/actions/CardActionCreators.test.js
+++ ReactTests/Tests/actions/CardActionCreators.test.js
@@ -3,14 +3,18 @@ import MockPromise from '../../mocks/MockPromise';
 
 import constants from '../../../ReactWebPack.CoreRC2/app/constants';
 
-describe('CardActionCreators, success', () => {
+describe.only('CardActionCreators, success', () => {
     const mockKanbanAPISuccess = {
         fetchCards() {
             return new MockPromise(true)
         },
         addCard(card) {
-            return new MockPromise(true, {fromServer: 'server side'})
+            return new MockPromise(true, { fromServer: 'server side' })
+        },
+        updateCard(card, draftCard) {
+            return new MockPromise(true, { fromServer: 'server side' })
         }
+        
     };
 
     let inject = require('inject?../api/KanbanApi!../../../ReactWebPack.CoreRC2/app/actions/CardActionCreators');
@@ -18,33 +22,50 @@ describe('CardActionCreators, success', () => {
         '../api/KanbanApi': mockKanbanAPISuccess
     }).default;
 
-    describe('fetchCards', () => {
-
-        it('success', () => {
-            let returnedActionObjects = [];
-            cardActions.fetchCards()((actionObject) => {
-                returnedActionObjects.push( actionObject);
-            });
-            expect(returnedActionObjects.length).toEqual(2);
-            expect(returnedActionObjects[0].type).toEqual(constants.FETCH_CARDS);
-            expect(returnedActionObjects[1].type).toEqual(constants.FETCH_CARDS_SUCCESS);
-            expect(returnedActionObjects[1].payload.response).toEqual('my data');
+    it('fetchCards', () => {
+        let returnedActionObjects = [];
+        cardActions.fetchCards()((actionObject) => {
+            returnedActionObjects.push(actionObject);
         });
+        expect(returnedActionObjects.length).toEqual(2);
+        expect(returnedActionObjects[0].type).toEqual(constants.FETCH_CARDS);
+        expect(returnedActionObjects[1].type).toEqual(constants.FETCH_CARDS_SUCCESS);
+        expect(returnedActionObjects[1].payload.response).toEqual('my data');
     });
 
-    describe('addCard', () => {
 
-        it('success', () => {
-            let returnedActionObjects = [];
-            cardActions.addCard({ fromReact: 'client side'})((actionObject) => {
-                returnedActionObjects.push( actionObject);
-            });
-            debugger;
-            expect(returnedActionObjects.length).toEqual(2);
-            expect(returnedActionObjects[0].type).toEqual(constants.CREATE_CARD);
-            expect(returnedActionObjects[1].type).toEqual(constants.CREATE_CARD_SUCCESS);
-            expect(returnedActionObjects[1].payload.card.fromReact).toEqual('client side');
-            expect(returnedActionObjects[1].payload.response.fromServer).toEqual('server side');
+    it('addCard', () => {
+        let returnedActionObjects = [];
+        cardActions.addCard({ fromReact: 'client side' })((actionObject) => {
+            returnedActionObjects.push(actionObject);
         });
+        expect(returnedActionObjects.length).toEqual(2);
+        expect(returnedActionObjects[0].type).toEqual(constants.CREATE_CARD);
+        expect(returnedActionObjects[1].type).toEqual(constants.CREATE_CARD_SUCCESS);
+        expect(returnedActionObjects[1].payload.card.fromReact).toEqual('client side');
+        expect(returnedActionObjects[1].payload.response.fromServer).toEqual('server side');
     });
+
+    it('updateCard', () => {
+        let returnedActionObjects = [];
+        cardActions.updateCard({ id: 'org card id' }, { newTitle: 'new title' })((actionObject) => {
+            returnedActionObjects.push(actionObject);
+        });
+        expect(returnedActionObjects.length).toEqual(2);
+        expect(returnedActionObjects[0].type).toEqual(constants.UPDATE_CARD);
+        expect(returnedActionObjects[1].type).toEqual(constants.UPDATE_CARD_SUCCESS);
+        expect(returnedActionObjects[1].payload.card.id).toEqual('org card id');
+        expect(returnedActionObjects[1].payload.draftCard.newTitle).toEqual('new title');
+        expect(returnedActionObjects[1].payload.response.fromServer).toEqual('server side');
+    });
+
+    it('updateCardStatus', () => {
+        let returnedActionObjects = [];
+        var result = cardActions.updateCardStatus(1, 2);
+        expect(result.type).toEqual(constants.UPDATE_CARD_STATUS);
+        expect(result.payload.cardId).toEqual(1);
+        expect(result.payload.listId).toEqual(2);
+        expect(result.meta.throttle).toEqual(true);     //this causes the action to be throttled by Redux middleware
+    });
+
 }); 
